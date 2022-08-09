8e7b80404215a589692cf8e9d1b281f8772ec894
SapphireBlueSkittlez <angelinespo@gmail.com>
2022-08-09 16:52:41 -0400

    Add reset button and prompt for user-defined grid

--- script.js
+++ script.js
@@ -1,24 +1,39 @@
 let container = document.getElementById('container');
-
+let resetBtn = document.getElementById('reset');
 let numGrid = 16;
 
-//container.style.backgroundColor = 'red';
+resetBtn.addEventListener('click', resetGrid);
 
-for(let i = 0; i < numGrid; i++) {
-    let row = document.createElement('div');
-    row.className = 'rowContainer';
-    row.style.display = 'block';
-    for(let j = 0; j < numGrid; j++) {
-        let div = document.createElement('div');
-        div.className = 'grid';
-        div.style.backgroundColor = '#f3f3f3';
-        div.style.height = (960 / numGrid) + 'px';
-        div.style.width = (960 / numGrid) + 'px';
-        //div.textContent = i + " : " + j;
-        div.addEventListener('mouseenter', () => {
-            div.style.backgroundColor = 'black';
-        });
-        row.appendChild(div);
+function resetGrid() {
+    let numSquares = prompt('How many pixels for the grid?', 'enter a number less than 100');
+    removeGrid(container);
+    createGrid(Number(numSquares));
+}
+
+function createGrid(numSquares) {
+    for(let i = 0; i < numSquares; i++) {
+        let row = document.createElement('div');
+        row.className = 'rowContainer';
+        row.style.display = 'block';
+        for(let j = 0; j < numSquares; j++) {
+            let div = document.createElement('div');
+            div.className = 'grid';
+            div.style.backgroundColor = '#f3f3f3';
+            div.style.height = (960 / numSquares) + 'px';
+            console.log(div.style.height);
+            div.style.width = (960 / numSquares) + 'px';
+            //div.textContent = i + " : " + j;
+            div.addEventListener('mouseenter', () => {
+                div.style.backgroundColor = 'black';
+            });
+            row.appendChild(div);
+        }
+        container.appendChild(row);
     }
-    container.appendChild(row);
+}
+
+function removeGrid(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
 }
